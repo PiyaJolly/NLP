6a867b235b621be6bbd43f0f7a69a4c2af1a4d69
Pragya Roy <pragyaparamita.roy@gmail.com>
2024-05-08 19:36:51 +1000

    modified:   model.py

--- model.py
+++ model.py
@@ -33,12 +33,12 @@ nltk.download('stopwords')
 
 def clean_text(text):
     text = str(text).lower()
-    # text = re.sub('\[.*?\]', '', text)
-    # text = re.sub('https?://\S+|www\.\S+', '', text)
-    # text = re.sub('<.*?>+', '', text)
-    # text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
-    # text = re.sub('\n', '', text)
-    # text = re.sub('\w*\d\w*', '', text)
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
     return text
 
 def remove_stopwords(text):
